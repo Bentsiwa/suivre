3d7289d2658acb0db79553d9c9c1c65c1ea44f90
Bentsiwa <efuabainson@gmail.com>
2017-01-25 12:36:44 +0000

    push notifications

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@
 	
 	function onDeviceReady() {
 
-      
+      alert("here");
 		var push = PushNotification.init({ "android": {"senderID": "81511517930"}});
 		 push.on('registration', function(data) {
+		 var device=data.registrationId; 
 		 console.log(data.registrationId);
-		 document.getElementById("gcm_id").innerHTML = data.registrationId;
+		 //document.getElementById("gcm_id").innerHTML = data.registrationId;
+		 alert(data.registrationId);
 		 });
 
 		 push.on('notification', function(data) {
